e691b48ac7bceb9b8abb46b2122c350a940893b2
Omar <omartck1620@gmail.com>
2020-07-06 21:48:19 -0500

    Se guarda el tema en el localStorage

--- js/modo_oscuro.js
+++ js/modo_oscuro.js
@@ -3,20 +3,37 @@
 const ThemeBtn = document.querySelector(".dark-theme-btn"),
   selectors = document.querySelectorAll("[dark-theme]");
 
-console.log(selectors);
+//console.log(selectors);
 
 let moon = "🌙",
   sun = "☀️";
 
+const lightTheme = () => {
+  selectors.forEach((el) => el.classList.remove("dark-mode"));
+  ThemeBtn.textContent = moon;
+  localStorage.setItem("theme", "light");
+};
+
+const darkTheme = () => {
+  selectors.forEach((el) => el.classList.add("dark-mode"));
+  ThemeBtn.textContent = sun;
+  localStorage.setItem("theme", "dark");
+};
+
 ThemeBtn.addEventListener("click", (e) => {
   if (e.target.matches(".dark-theme-btn")) {
-    console.log(ThemeBtn.textContent);
+    //console.log(ThemeBtn.textContent);
     if (ThemeBtn.textContent === moon) {
-      selectors.forEach((el) => el.classList.add("dark-mode"));
-      ThemeBtn.textContent = sun;
+      darkTheme();
     } else {
-      selectors.forEach((el) => el.classList.remove("dark-mode"));
-      ThemeBtn.textContent = moon;
+      lightTheme();
     }
   }
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log(localStorage.getItem("theme"));
+  if (localStorage.getItem("theme") === null) ls.setItem("theme", "light");
+  if (localStorage.getItem("theme") === "light") lightTheme();
+  if (localStorage.getItem("theme") === "dark") darkTheme();
 });
